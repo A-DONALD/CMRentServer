74bad9c9285043f9a2b5509999a80aa5d19e7a0f
Y-DONALD <donaldyoungoua@gmail.com>
2024-02-29 20:44:29 +0100

    configure logs and middleware

---
+++ middleware/notFound.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const notFound = (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, '..', 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" })
+    } else {
+        res.type('txt').send("404 Not Found")
+    }
+}
+
+module.exports = notFound;
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const errorHandler = require('./middleware/errorHandler');
+const notFound = require('./middleware/notFound');
+const { logger } = require('./middleware/logEvents');
+const PORT = process.env.PORT || 3500;
 
-//create an instance of events
-class MyEmitter extends EventEmitter { };
-// initialize
-const myEmitter = new MyEmitter();
-// add the listener
-myEmitter.on('log', (msg) => logEvents(msg));
+// Save the log request
+app.use(logger);
+// Cross Origin Resource Sharing
+const whitelist = ['https://a-donald.vercel.app', 'http://127.0.0.1:5173', 'http://127.0.0.1:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not Allowed by CORS'))
+        }
+    },
+    optionSuccesStatus: 200
+}
+app.use(cors(corsOptions));
 
-setTimeout(() => {
-    myEmitter.emit('log', 'Log event emitted!');
-}, 4000);
+// Middleware to handle url encoded data like forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for json data
+app.use(express.json());
+// server static files directory
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
+})
+// defaut route
+app.all('*', notFound)
+// error handler
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server listenning on port ${PORT}`));
